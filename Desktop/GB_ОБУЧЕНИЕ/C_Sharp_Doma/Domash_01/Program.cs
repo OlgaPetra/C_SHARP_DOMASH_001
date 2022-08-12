cbe5fc618a9212215da780a81195541d8a595baf
Olya <pradicha@mail.ru>
2022-08-12 17:58:15 +0300

    условие задачи 4

--- Desktop/GB_ОБУЧЕНИЕ/C_Sharp_Doma/Domash_01/Program.cs
+++ Desktop/GB_ОБУЧЕНИЕ/C_Sharp_Doma/Domash_01/Program.cs
@@ -6,26 +6,38 @@ Console.Clear();
 //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 
-Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
-int a, b;
-Console.WriteLine("Введи первое число - ");
+// Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
+// int a, b;
+// Console.WriteLine("Введи первое число - ");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введи второе число - ");
+// b = Convert.ToInt32(Console.ReadLine());
+
+//     bool Chto0 = a > b;
+//     bool Chto1 = a == b;
+//     bool Chto2 = a < b;
+
+// if (Chto0)
+// {
+//     Console.WriteLine($"Число {a} больше чем {b} ");
+// }
+//     else if (Chto1)
+//     {
+//     Console.WriteLine($"Числа {a} и {b} равны");
+//     }
+//     else
+//     {
+//     Console.WriteLine($"Число {b} больше чем {a}");
+//     }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введи 3 числа и мы тебе скажем какое из них максимальное");
+int a, b, c;
+onsole.WriteLine("Введи первое число - ");
 a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введи второе число - ");
+Console.WriteLine("Введи первое число -");
 b = Convert.ToInt32(Console.ReadLine());
- 
-    bool Chto0 = a > b;
-    bool Chto1 = a == b;
-    bool Chto2 = a < b;
- 
-    if (Chto0)
-    {
-        Console.WriteLine($"Число {a} больше чем {b} ");
-    }
-        else if (Chto1)
-        {
-        Console.WriteLine($"Числа {a} и {b} равны");
-        }
-        else
-        {
-        Console.WriteLine($"Число {b} больше чем {a}");
-        }
+Console.WriteLine("Введи первое число - ");
+c = Convert.ToInt32(Console.ReadLine());
+
